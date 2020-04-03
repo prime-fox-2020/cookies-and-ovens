7c66e7b2226947e46ddc2bc53ed98961d271bb61
charles2901 <chrlsjnthn@gmail.com>
2020-04-03 15:41:20 +0700

    edit solution

--- index.js
+++ index.js
@@ -18,13 +18,14 @@
 /*RELEASE 0 - 1 */
 
 // class Kue{
-//     constructor(bakingTime){
+//     constructor(bakingTime, lamaBaking){
 //         this.bakingTime = bakingTime
+//         this.lamaBaking = lamaBaking
 //     }
 //     bakingProgress(interval){
 //         let time = 0
 //         let status
-//         do{
+//         while(time < this.lamaBaking){
 //             time += interval
 //             //Asumsi bahwa status kue hampir matang saat waktu panggang telah melebih 2/3 dari waktu kue seharusnya matang
 //             if(time <=  2/3*this.bakingTime) status = 'mentah'
@@ -38,24 +39,23 @@
 //                 console.log(`Kue ${this.flavor.rasa}, menit ke-${time} : ${status}`)
 //             }
 //         }
-//         while(time <= this.bakingTime)
 //     }
 // }
 // class KueCoklat extends Kue{
-//     constructor(bakingTime, flavor){
-//         super(bakingTime)
+//     constructor(bakingTime, lamaBaking, flavor){
+//         super(bakingTime, lamaBaking)
 //         this.flavor = new Bahan('cokelat')
 //     }
 // }
 // class KueKacang extends Kue{
-//     constructor(bakingTime, flavor){
-//         super(bakingTime)
+//     constructor(bakingTime, lamaBaking, flavor){
+//         super(bakingTime, lamaBaking)
 //         this.flavor = new Bahan('kacang')
 //     }
 // }
 // class KueKeju extends Kue{
-//     constructor(bakingTime, flavor){
-//         super(bakingTime)
+//     constructor(bakingTime, lamaBaking, flavor){
+//         super(bakingTime, lamaBaking)
 //         this.flavor = new Bahan('keju')
 //     }
 // }
@@ -65,15 +65,15 @@
 //     }
 // }
 
-// let kueCoklat = new KueCoklat(20)
+// let kueCoklat = new KueCoklat(20, 25)
 // kueCoklat.bakingProgress(5)
 // console.log('\n')
 
-// let kueKacang = new KueKacang(25)
+// let kueKacang = new KueKacang(25, 30)
 // kueKacang.bakingProgress(5)
 // console.log('\n')
 
-// let kueKeju = new KueKeju(30)
+// let kueKeju = new KueKeju(30, 35)
 // kueKeju.bakingProgress(5)
 
 
@@ -81,14 +81,15 @@
     Refactor code di atas dengan satu kelas dengan menggunakan static method dari kelas Kue dengan menerima input flavor
 */
 class Kue{
-    constructor(bakingTime, flavor){
+    constructor(bakingTime, lamaBaking, flavor){
         this.bakingTime = bakingTime
+        this.lamaBaking = lamaBaking
         this.flavor = flavor
     }
     bakingProcess(interval){
         let time = 0
         let status
-        do{
+        while(time < this.lamaBaking){
             time += interval
             //Asumsi bahwa status kue hampir matang saat waktu panggang telah melebih 2/3 dari waktu kue seharusnya matang
             if(time <=  2/3*this.bakingTime) status = 'mentah'
@@ -102,15 +103,14 @@ class Kue{
                 console.log(`Kue ${this.flavor}, menit ke-${time} : ${status}`)
             }
         }
-        while(time <= this.bakingTime)
     }
     static bakingCakeWithFlavor(bakingTime, flavor){
         return new Kue(bakingTime, flavor)
     }
 }
 
-Kue.bakingCakeWithFlavor(20,'coklat').bakingProcess(5)
+Kue.bakingCakeWithFlavor(20,25,'coklat').bakingProcess(5)
 console.log('\n')
-Kue.bakingCakeWithFlavor(25,'kacang').bakingProcess(5)
+Kue.bakingCakeWithFlavor(25,30,'kacang').bakingProcess(5)
 console.log('\n')
-Kue.bakingCakeWithFlavor(30,'keju').bakingProcess(5)
+Kue.bakingCakeWithFlavor(30,35,'keju').bakingProcess(5)
