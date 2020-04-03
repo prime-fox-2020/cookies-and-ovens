ef11b71549fea3108256778ee33679e5705df768
riolly <albert.r.cahyantara@gmail.com>
2020-04-03 17:58:27 +0700

    Release 0: Design the structure

--- index.js
+++ index.js
@@ -6,5 +6,47 @@
 // - How will the classes interact with each other?
 // - Which classes will inherit from others, if any?
 //
-//
-// Your code here
+
+class Cookies {
+    constructor(flavour) {
+        this._flavour = flavour;
+        this._cookTime = this.timeToCook();
+    }
+    get flavour() {return this._flavour;}
+    set flavour(param) {this._flavour = param;}
+    get cookTime() {return this._cookTime;}
+    set cookTime(param) {this._cookTime = param;}
+
+    timeToCook() {
+        switch (this.flavour) {
+            case  'coklat':
+                return 20;
+            case 'kacang':
+                return 30; 
+            case 'keju':
+                return 35; 
+        }
+    }
+}
+
+class Cook {
+    constructor() {
+        this._nowTime = 0;
+    }
+    get nowTime() {return this._nowTime;}
+    set nowTime(param) {this._nowTime = param;}
+    
+    static cookies(flavour, setTime) {
+        let baking = new Cookies(flavour);
+        if (setTime > baking.cookTime){
+            console.log(`Kue ${flavour} gosong`);
+        } else if (setTime == baking.cookTime) {
+            console.log(`Kue ${flavour} matang`);
+        } else {
+            console.log(`Kue ${flavour} mentah`);
+        }
+    }
+    
+}
+
+Cook.cookies('keju', 34);
