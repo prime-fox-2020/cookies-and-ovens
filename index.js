40bb6a82850a7d20e85f99dc33ef459fca087afa
SonyMa <sonymartha4@gmail.com>
2020-04-03 22:14:10 +0700

    complete

--- index.js
+++ index.js
@@ -8,3 +8,55 @@
 //
 //
 // Your code here
+class Kue {
+    constructor(namaKue) {
+        this.kue = namaKue
+    }
+
+    cook() {
+        if (this.kue === 'coklat') {
+            let cookTime = 20
+            for(let i = 0; i <= cookTime; i+=5) {
+                if(i === cookTime){
+                    console.log(`Kue ${this.kue}, menit ke ${i} : matang`)
+                    console.log(`Kue ${this.kue}, menit ke ${i+5} : hangus`)
+                } else if(i === cookTime-5){
+                    console.log(`Kue ${this.kue}, menit ke ${i} : hampir matang`)
+                } else{
+                    console.log(`Kue ${this.kue}, menit ke ${i} : mentah`)
+                }
+            }
+
+        }
+        else if (this.kue === 'kacang') {
+            let cookTime = 30
+            for(let i = 0; i <= cookTime; i+=5) {
+                if(i === cookTime){
+                    console.log(`Kue ${this.kue}, menit ke ${i} : matang`)
+                    console.log(`Kue ${this.kue}, menit ke ${i+5} : hangus`)
+                } else if(i === cookTime-5){
+                    console.log(`Kue ${this.kue}, menit ke ${i} : hampir matang`)
+                } else{
+                    console.log(`Kue ${this.kue}, menit ke ${i} : mentah`)
+                }
+            }
+        }
+        else if (this.kue === 'keju') {
+            let cookTime = 35
+            for(let i = 0; i <= cookTime; i+=5) {
+                if(i === cookTime){
+                    console.log(`Kue ${this.kue}, menit ke ${i} : matang`)
+                    console.log(`Kue ${this.kue}, menit ke ${i+5} : hangus`)
+                } else if(i === cookTime-5){
+                    console.log(`Kue ${this.kue}, menit ke ${i} : hampir matang`)
+                } else{
+                    console.log(`Kue ${this.kue}, menit ke ${i} : mentah`)
+                }
+            }
+        }
+    }
+}
+
+let kue = new Kue('keju')
+kue.cook()
+// console.log(kue)
