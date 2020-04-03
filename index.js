e14d80830dc23536f2aaf43337b2d384eb1904e7
fadhlanfm <fafadhlanriz@gmail.com>
2020-04-03 23:28:08 +0700

    Release 0: OK

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // Answer These Questions:
 //
 // - What are essential classes?
+//      Kue, Coklat, Kacang, Keju, Oven
 // - What attributes will each class have?
+//      Kue, Coklat, Kacang, Keju: nama, butuhWaktu, status
+//      Oven: isi, waktu, status
 // - What interface will each class provide?
+//      Oven dapat menampilkan isi kue yang sedang dimasak. Dan setiap 5 menit menampilkan status kematangan kue tersebut
 // - How will the classes interact with each other?
+//      Kue adalah parent dari Coklat, Kacang, Keju
+//      Children dari Kue adalah composition untuk Oven
 // - Which classes will inherit from others, if any?
-//
-//
+//      Coklat, Kacang, Keju mewarisi Kue
+
+
 // Your code here
