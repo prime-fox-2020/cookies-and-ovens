cdc9043dcab9c0def235e5254d5ae348529b25f6
Vincent Guizot <30890039+CenZo128@users.noreply.github.com>
2020-04-03 19:18:02 +0700

    RALAT

--- index.js
+++ index.js
@@ -1,175 +1,87 @@
-"use strict"
+// Answer These Questions:
+//
+// - What are essential classes?
+//  Cookies and Oven
+// - What attributes will each class have?
+//  Cookies : namaKue, menit, status ; Oven : cookie, lamaMasak, statusOven
+// - What interface will each class provide?
+//  Cookies has description method, Oven has masakKue method
+// - How will the classes interact with each other?
+//  Child extends form its Parent
+// - Which classes will inherit from others, if any?
+//  Coklat, Kacang, Keju from Cookies
 
-class Ingredient {
-    constructor(options) {
-        this.name = options['name']
-        this.amount = options['amount']
+
+class Cookies {
+    constructor(namaKue, menit) {
+        this.namaKue = namaKue
+        this.menit = menit
+        this.status = 'Mentah'
+    }
+
+    description() {
+        let message = `${this.namaKue} : ${this.menit} menit`
+        return message
     }
 }
 
-class Cookie {
-    constructor(name, ingredients = []) {
-        this.name = name
-        this.status = "mentah"
-        this.ingredients = this.getRecipe(ingredients)
 
+class Oven {
+    constructor(cookie, lamaMasak) {
+        this.cookie = cookie
+        this.lamaMasak = lamaMasak
+        this.statusOven = ''
     }
-    getRecipe(ingredients) {
-        let arr = []
-        for (let i = 0; i < ingredients.length; i++) {
-            arr.push(new Ingredient(ingredients[i]))
+
+    masakKue() {
+        console.log(`\nOven Sedang memasak ${this.cookie.namaKue}, ${this.cookie.status}`);
+        for (let i = 0; i <= this.lamaMasak; i += 5) {
+            if (i < this.cookie.menit / 2) {
+                this.statusOven = 'belum matang'
+                console.log(`- Menit ke ${i}: ${this.statusOven}`);
+            } else if (i > this.cookie.menit / 2 && i < this.cookie.menit) {
+                this.statusOven = 'hampir matang'
+                console.log(`- Menit ke ${i}: ${this.statusOven}`);
+            } else if (i == this.cookie.menit) {
+                this.statusOven = 'matang'
+                console.log(`- Menit ke ${i}: ${this.statusOven}`);
+            } else if (i > this.cookie.menit) {
+                this.statusOven = 'gosong'
+                console.log(`- Menit ke ${i}: ${this.statusOven}`);
+            } 
         }
-        return arr
-    }
+        this.cookie.status = this.statusOven
+        console.log(`Status: ${this.cookie.namaKue}, ${this.cookie.status}`);
 
-    // get status(){
-    // return this.status
-    // }
-    bake() {
-        this.status = "selesai dimasak"
-    }
-}
-
-class PeanutButter extends Cookie {
-    constructor(name, ingredients = []) {
-        super(name, ingredients)
-        this.peanut_count = 100
     }
 
 }
 
-class ChocolateChip extends Cookie {
-    constructor(name, ingredients = []) {
-        super(name, ingredients)
-        this.choc_chip_count = 200
+class Coklat extends Cookies {
+    constructor (namaKue, menit) {
+        super(namaKue, menit) 
+    }
+
+}
+
+class Kacang extends Cookies {
+    constructor (namaKue, menit) {
+        super(namaKue, menit)
     }
 }
 
-class ChocolateCheese extends Cookie {
-    constructor(name, ingredients = []) {
-        super(name, ingredients)
-        this.choc_chip_count = 150
+class Keju extends Cookies {
+    constructor (namaKue, menit) {
+        super(namaKue, menit) 
     }
 }
 
-class ChocolateButter extends Cookie {
-    constructor(name, ingredients = []) {
-        super(name, ingredients)
-        this.peanut_count = 100
-    }
-}
-class OtherCookie extends Cookie {
-    constructor(name, ingredients = []) {
-        super(name, ingredients)
-        this.peanut_count = 100
-    }
-}
+let kueCoklat = new Coklat('Kue Coklat', 20)
+let kueKacang = new Kacang('Kue Kacang', 30)
+let kueKeju = new Keju('Kue Keju', 35) 
 
-// Read File Sync
-let fs = require('fs')
-let options = fs.readFileSync('cookies.txt', 'utf8').split('\r\n')
-let options_ingredients = fs.readFileSync('cookies_ingredients.txt', 'utf8').split('\r\n')
-
-
-class CookieFactory {
-    static create(cookies) {
-        // accepts a list of cookie types and returns those cookies.txt
-        // Tambahin othercookie
-        let cookiesType = []
-        let cookieResep = this.getIngredients()
-        // console.log(cookieResep)
-        for (let i = 0; i < cookies.length; i++) {
-            switch (cookies[i]) {
-                case 'peanut butter':
-                    cookiesType.push(new PeanutButter(cookies[i], cookieResep[i]))
-
-                    break
-                case 'chocolate chip':
-                    cookiesType.push(new ChocolateChip(cookies[i], cookieResep[i]))
-
-                    break
-                case 'chocolate cheese':
-                    cookiesType.push(new ChocolateCheese(cookies[i], cookieResep[i]))
-
-                    break
-                case 'chocolate butter':
-                    cookiesType.push(new ChocolateButter(cookies[i], cookieResep[i]))
-
-                    break
-                default:
-                    cookiesType.push(new OtherCookie(cookies[i], cookieResep[i]))
-                    break
-
-            }
-        }
-
-        return cookiesType;
-    }
-
-    static getIngredients() {
-        //Pecah per type
-        let ingredients_amount = []
-        let x = []
-        for (let i = 0; i < options_ingredients.length; i++) {
-            x = []
-            x = options_ingredients[i].split(' = ')
-            ingredients_amount.push(x[1].split(','))
-        }
-        //Pecah per amount
-        let ingredients_amount_details = [], temp = [], temp1 = [], obj = {}
-        for (let i = 0; i < ingredients_amount.length; i++) {
-            temp1 = []
-            for (let j = 0; j < ingredients_amount[i].length; j++) {
-                temp = []
-                temp = ingredients_amount[i][j].split(' : ')
-                temp1.push(temp)
-            }
-            ingredients_amount_details.push(temp1)
-        }
-        //Jadiin Objek
-        let ingredients_array = [], ingredients_array_1 = []
-        for (let i = 0; i < ingredients_amount_details.length; i++) {
-            ingredients_array = []
-
-            for (let j = 0; j < ingredients_amount_details[i].length; j++) {
-                obj = {}
-                obj["name"] = ingredients_amount_details[i][j][1]
-                obj["amount"] = ingredients_amount_details[i][j][0]
-                ingredients_array.push(obj)
-            }
-            ingredients_array_1.push(ingredients_array)
-        }
-        return ingredients_array_1
-    }
-    static cookieRecommendations(string,x){
-        let lessSugar=[]
-        let cookieLessSugar = x,check=false
-        for(let i=0;i<cookieLessSugar.length;i++){
-            check=false
-            let temp=[]
-            temp=cookieLessSugar[i]['ingredients']
-
-            for(let j=0;j<temp.length;j++){
-                if(temp['name']=='sugar'){
-                    check=true
-                    break
-                }
-            }
-            if(check==false){
-                lessSugar.push(cookieLessSugar[i]['name'])
-            }
-        }
-
-        // console.log(cookieLessSugar)
-        return lessSugar
-    }
-}
-
-let batch_of_cookies = CookieFactory.create(options)
-console.log(batch_of_cookies)
-let sugarFreeFoods = CookieFactory.cookieRecommendations("Tuesdays", batch_of_cookies)
-
-console.log("Sugar free cakes are : ", sugarFreeFoods)
-    
-
-// const pb = CookieFactory.create('peanut_butter') // PeanutButter
+let cookiesReadyOven = [kueCoklat, kueKacang, kueKeju]
+for (let i = 0; i < cookiesReadyOven.length; i++) {
+    let masukanKeOven = new Oven(cookiesReadyOven[i], 40)    
+    masukanKeOven.masakKue()
+} 
