ab611e4db2c5499d2c0fc1c8f42095867158162c
Sujackson <su_jackson99@yahoo.com>
2020-04-03 22:24:27 +0700

    done

--- index.js
+++ index.js
@@ -8,3 +8,59 @@
 //
 //
 // Your code here
+class Kue {
+    constructor(waktu, target, rasa){
+        this.waktu=waktu
+        this.target=target
+        this.rasa=rasa
+    }
+    proses(cek){
+        let time =0
+        let cook=''
+        do {
+            time+=cek
+            if(time<=(0.5*this.waktu)){
+                cook='mentah'
+            }else if (time>=(0.5*this.waktu)&&time<this.target){
+                cook='hampir matang'
+            }else if (time==this.target){
+                cook='matang'
+            }else if (time>this.target){
+                cook='hangus'
+            }
+            if(this.rasa==undefined){
+                console.log(`Kue ini , menit ke-${time} : ${cook}`)
+            }else{
+                console.log(`Kue ${this.rasa}, menit ke-${time} : ${cook}`)
+            }
+        } while (time<this.waktu);
+    }
+}
+
+class Coklat extends Kue{
+    constructor(waktu,target,rasa){
+        super(waktu,target,rasa)
+    }
+}
+
+class Kacang extends Kue{
+    constructor(waktu,target,rasa){
+        super(waktu,target,rasa)
+    }
+}
+
+class Keju extends Kue {
+    constructor(waktu,target,rasa){
+        super(waktu,target,rasa)
+    }
+}
+
+const coklat= new Coklat(25,20,'coklat')
+coklat.proses(5)
+
+
+const kacang= new Kacang(35,30,'kacang')
+kacang.proses(5)
+
+const keju= new Keju(40,35,'keju')
+keju.proses(5)
