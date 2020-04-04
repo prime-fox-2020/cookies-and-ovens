f8c5ca87a1f7b7abe783d3bee10e9250e11becd6
Eko Prasetyo <ekoprasetyo.me@gmail.com>
2020-04-04 16:37:19 +0700

    release 0

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // Answer These Questions:
 //
 // - What are essential classes?
+//   Class Kue, Oven, KueCoklat, KueKacang, KueKeju
 // - What attributes will each class have?
+//   Kue's attributes:
+//   -  _name, _timeToCook, _ingredient
+//   Oven's attributes:
+//   -  
 // - What interface will each class provide?
+//   - bake()
 // - How will the classes interact with each other?
+//   Oven menerim bahan, dan waktu memanggang untuk menciptakan kue
 // - Which classes will inherit from others, if any?
-//
+//   KueCoklat, KueKacang, KueKeju
 //
 // Your code here
